f8f2bb105da319fc4e4ce8e3791beba6b7b65431
Tamilselvan Shanmugam <yestamilselvan@gmail.com>
2020-04-11 21:03:19 +0200

    Update script

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,10 +1,9 @@
 
-function setWidthFbPage() {
-	var container_width = $('#fb-page-wrapper').width();    
-    $('.fb-page').attr("data-width", container_width);
-    FB.XFBML.parse();   
-    console.log( container_width);
-}
-
-$( window ).resize(setWidthFbPage);
- window.onload = setWidthFbPage;
+document.addEventListener("DOMContentLoaded", function(event) {
+	$(window).bind("load resize", function(){
+		setTimeout(function() {
+			$("#fb-page-wrapper .fb-page").attr("data-width", parseInt($("#fb-page-wrapper").width()));
+			FB.XFBML.parse();
+		}, 100);
+	});
+});
